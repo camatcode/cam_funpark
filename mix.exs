32dce0c6fa9baed675e3db3998ec986fe0a5fc55
Cam Cook <cam.cook.codes@gmail.com>
2025-09-02 12:58:43 -0400

    build: deps

---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule FunPark.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :adv_func,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
+      {:ex_license, "~> 0.1", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_machina, "~> 2.8", only: :test},
+      {:faker, "~> 0.18", only: :test},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:quokka, "~> 2.8", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
---
+++ test/adv_func_test.exs
@@ -0,0 +1,9 @@
+defmodule FunParkTest do
+  use ExUnit.Case
+
+  doctest FunPark
+
+  test "greets the world" do
+    assert FunPark.hello() == :world
+  end
+end
