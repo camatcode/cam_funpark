4781f53fc418ef9abb79584516f613c3fa1ae32b
Cam Cook <cam.cook.codes@gmail.com>
2025-09-02 23:50:29 -0400

    feat: End of Chapter 2

--- lib/fun_park/list.ex
+++ lib/fun_park/list.ex
@@ -12,4 +12,37 @@ defmodule FunPark.List do
     end)
     |> Enum.reverse()
   end
+
+  def union(list1, list2, eq \\ FunPark.Eq) when is_list(list1) and is_list(list2) do
+    (list1 ++ list2) |> uniq(eq)
+  end
+
+  def intersection(list1, list2, eq \\ FunPark.Eq) when is_list(list1) and is_list(list2) do
+    list1
+    |> Enum.filter(fn item ->
+      Enum.any?(list2, &Utils.eq?(item, &1, eq))
+    end)
+    |> uniq()
+  end
+
+  def difference(list1, list2, eq \\ FunPark.Eq) when is_list(list1) and is_list(list2) do
+    list1
+    |> Enum.reject(fn item ->
+      Enum.any?(list2, &Utils.eq?(item, &1, eq))
+    end)
+    |> uniq()
+  end
+
+  def symmetric_difference(list1, list2, eq \\ FunPark.Eq) when is_list(list1) and is_list(list2) do
+    (difference(list1, list2, eq) ++ difference(list2, list1, eq))
+    |> uniq()
+  end
+
+  def subset?(small, large, eq \\ FunPark.Eq) when is_list(small) and is_list(large) do
+    Enum.all?(small, fn item -> Enum.any?(large, &Utils.eq?(item, &1, eq)) end)
+  end
+
+  def superset?(large, small, eq \\ FunPark.Eq) when is_list(small) and is_list(large) do
+    subset?(small, large, eq)
+  end
 end
